63866c1025de38e113ab2e5c30d5c8d4f6ae6e39
Vincent Pacquet <vincent.pacquet@aerobace.com>
2018-01-02 14:42:03 +0100

    sign-in-ui-custom

---
+++ albator/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  albator
+//
+//  Created by Vincent Pacquet on 1/2/18.
+//  Copyright © 2018 aerobace. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
+
